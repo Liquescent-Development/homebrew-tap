8acd52d4576948bd94dfb834603c1076045427a9
Richard Kiene <richard@liquescent.dev>
2026-07-28 11:32:16 -0700

    gascan 0.1.11

--- Casks/gascan.rb
+++ Casks/gascan.rb
@@ -1,6 +1,6 @@
 cask "gascan" do
-  version "0.1.10"
-  sha256 "585c6087779f2cf545f65707ce56198c12deb8d1745a5cfaf5eafe7f09d7894a"
+  version "0.1.11"
+  sha256 "552ba971553d0a15016dd593cbfa7d1f2fe6eb3438bfe4265f60a75c22a062fd"
 
   url "https://github.com/Liquescent-Development/gascan/releases/download/v#{version}/gascan-#{version}-macos-arm64.pkg"
   name "Gas Can"
